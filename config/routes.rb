8b20ee1486009cd9c8842acd3754fb1076040562
CesarOsses <cesar.osses@gmail.com>
2023-07-11 22:03:10 -0400

    terminando el ejercicio dia 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :products
   resources :categories
+
+  post "reations",                to:"reactions#user_reaction",               as:"user_reaction"
+  get "my_reactions",             to:"reactions#product_with_reactions",      as:"my_reactions"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
